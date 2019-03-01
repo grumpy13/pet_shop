42c9cc4ff6385d0d1dc56edd74551717b2a5eec0
Mariam Ali <mariamqali13@gmail.com>
2019-03-01 17:47:21 +0300

    for bumblebee

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 
+class Pet(models.Model):
+	SEX_CHOICES = (
+	("MALE", "Male"),
+	("FEMALE", "Female"),
+	)
+
+	name = models.CharField(max_length=120)
+	age = models.IntegerField()
+	sex = models.CharField(max_length=120, choices=SEX_CHOICES, default="FEMALE")
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+
+	def __str__(self):
+		return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,1 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Pet
+from .forms import PetForm
+
+def pet_list(request):
+    context = {
+        "pets": Pet.objects.all()
+    }
+    return render(request, 'pet_list.html', context)
+
+def pet_detail(request, pet_id):
+    context = {
+        "pet": Pet.objects.get(id=pet_id)
+    }
+    return render(request, 'pet_detail.html', context)
+
+def pet_create(request):
+    form = PetForm()
+    if request.method == "POST":
+        form = PetForm(request.POST, request.FILES)
+        if form.is_valid():
+            pet_obj= form.save()
+            return redirect('pet-details', pet_obj.id)
+    context = {
+        "form":form,
+    }
+    return render(request, 'create.html', context)
+
+def pet_update(request, pet_id):
+    pet_obj = Pet.objects.get(id=pet_id)
+    form = PetForm(instance=pet_obj)
+    if request.method == "POST":
+        form = PetForm(request.POST, request.FILES, instance=pet_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('pet-details', pet_obj.id)
+    context = {
+        "pet_obj": pet_obj,
+        "form":form,
+    }
+    return render(request, 'update.html', context)
+
+def pet_delete(request, pet_id):
+    pet_obj = Pet.objects.get(id=pet_id)
+    pet_obj.delete()
+    return redirect('pets-list')
