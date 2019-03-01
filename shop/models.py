80cfa9b25ed27f79dd68f8fadb280be2b702a3e0
Mariam Ali <mariamqali13@gmail.com>
2019-03-01 18:09:40 +0300

    test passed

--- shop/models.py
+++ shop/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Pet(models.Model):
 	SEX_CHOICES = (
-	("MALE", "Male"),
-	("FEMALE", "Female"),
+	("Male", "Male"),
+	("Female", "Female"),
 	)
 
 	name = models.CharField(max_length=120)
 	age = models.IntegerField()
-	sex = models.CharField(max_length=120, choices=SEX_CHOICES, default="FEMALE")
+	sex = models.CharField(max_length=120, choices=SEX_CHOICES, default="Female")
 	price = models.DecimalField(max_digits=5, decimal_places=2)
 
 	def __str__(self):
